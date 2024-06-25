114852d4c67df9e5f2804087a6090df80d4a144f
Janina21 <janina@janinadiers.de>
2024-06-25 10:00:42 +0200

    included preview mode, to show drawing after recognition inside svg

--- export.js
+++ export.js
@@ -1,4 +1,5 @@
 export function exportToInkML(traces) {
+    console.log('trace', traces);
     let inkML = '<?xml version="1.0" encoding="UTF-8"?>\n<ink>\n';
     inkML += '  <traceFormat>\n';
     inkML += '    <channel name="X" type="integer" />\n';
@@ -12,10 +13,56 @@ export function exportToInkML(traces) {
         inkML += '  </trace>\n';
     });
     inkML += '</ink>';
+    console.log(inkML);
     return inkML;
 }
 
-export function download(filename, text) {
+export function exportToPnml(shapes) {
+    
+    let circles = shapes[0]
+    let rectangles = shapes[1]
+    let lines = shapes[2]
+
+    console.log(circles, rectangles, lines);
+    
+    let pnmlString =
+        '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n<pnml>\n<net id="" type="http://www.pnml.org/version-2009/grammar/ptnet">\n<name>\n<text>ILovePetriNets.pnml</text>\n</name>\n<page id="p1">';
+    for (const circle of circles) {
+        pnmlString += `<place id="${circle.shape_id}">\n<name><text>${circle.shape_id}</text></name>\n<graphics>\n<position x="${circle.min_x}" y="${circle.min_y}"/>\n</graphics>\n<initialMarking>\n<text>0</text>\n</initialMarking>\n</place>`;
+    }
+
+    for (const rectangle of rectangles) {
+        pnmlString += `<transition id="${rectangle.shape_id}">\n<name><text>${rectangle.shape_id}</text></name>\n<graphics><position x="${rectangle.min_x}" y="${rectangle.min_y}"/>\n</graphics>\n</transition>`;
+    }
+
+    for (const line of lines) {
+        let graphics = '<graphics>\n';
+        // if (line.coords && line.coords.length > 0) {
+        //     for (const coord of line.coords) {
+        //         graphics += `<position x="${coord.x}" y="${coord.y}"/>\n`;
+        //     }
+        // }
+        graphics += '</graphics>\n';
+        pnmlString += `\n<arc id="${line.shape_id}" source="${line.source.id}" target="${line.target.id}">\n${graphics}\n<inscription><text>1</text></inscription>\n</arc>`;
+    }
+    pnmlString += '\n</page>\n</net>\n</pnml>';
+    return pnmlString;
+}
+
+export function downloadInkml(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+export function downloadPnml(filename, text) {
     var element = document.createElement('a');
     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
     element.setAttribute('download', filename);
--- script.js
+++ script.js
@@ -1,11 +1,39 @@
-import {exportToInkML, download} from './export.js';
+import {exportToInkML, downloadInkml, exportToPnml, downloadPnml} from './export.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     let canvas = document.getElementById('myCanvas');
-    let downloadButton = document.getElementById('download');
+    let svg = document.getElementById('svg');
     // Attach event listeners to buttons
     document.getElementById('zoomIn').addEventListener('click', zoomIn);
     document.getElementById('zoomOut').addEventListener('click', zoomOut);
+
+
+    let download_inkml_btn = document.createElement('button')
+    console.log('test');
+    download_inkml_btn.id = 'download_inkml_btn'
+    download_inkml_btn.textContent = 'Download in inkml Format'
+    document.querySelector('menu').appendChild(download_inkml_btn)
+    console.log(download_inkml_btn);
+    download_inkml_btn.addEventListener('click', function() {
+        let inkML = exportToInkML(traces);
+        downloadInkml('drawing.inkml', inkML);
+    });
+
+    let upload_inkml_btn = document.createElement('button')
+    upload_inkml_btn.id = 'upload_inkml_btn'
+    upload_inkml_btn.textContent = 'Upload inkml File'
+    document.querySelector('menu').appendChild(upload_inkml_btn)
+
+    upload_inkml_btn.addEventListener('click', function(){
+        document.getElementById('fileInput').click();
+    })
+
+    let previewButton = document.createElement('button');
+    previewButton.id = 'previewButton';
+    previewButton.textContent = 'Preview';
+    document.querySelector('menu').appendChild(previewButton);
+    
+    
 
     paper.setup(canvas);
     let tool = new paper.Tool();
@@ -22,15 +50,15 @@ document.addEventListener('DOMContentLoaded', function() {
         paper.view.scale(0.9, paper.view.center); // Zoom out by 10%
     }
 
-    
+    // // paper.view.setViewSize(canvas.getBoundingClientRect()['width'], canvas.getBoundingClientRect()['height']);
+    // if (window.devicePixelRatio) {
+    //             canvas.width = canvas.offsetWidth * window.devicePixelRatio;
+    //             canvas.height = canvas.offsetHeight * window.devicePixelRatio;
+    //             canvas.style.width = canvas.offsetWidth + 'px';
+    //             canvas.style.height = canvas.offsetHeight + 'px';
+    //             paper.view.setViewSize(canvas.width, canvas.height);
+    //         }
 
-    if (window.devicePixelRatio) {
-        canvas.width = canvas.offsetWidth * window.devicePixelRatio;
-        canvas.height = canvas.offsetHeight * window.devicePixelRatio;
-        canvas.style.width = canvas.offsetWidth + 'px';
-        canvas.style.height = canvas.offsetHeight + 'px';
-        paper.view.setViewSize(canvas.width, canvas.height);
-    }
 
     tool.onMouseDown = function(event) {
         // timeStamps.push(Date.now());
@@ -62,7 +90,8 @@ document.addEventListener('DOMContentLoaded', function() {
     tool.onMouseUp = function(event) {
         
         traces.push(path.segments.map(function(segment) {
-            return {x: Math.round(segment.point.x), y: Math.round(segment.point.y)};
+            console.log(segment.point.x, segment.point.y);
+            return {x: segment.point.x, y: segment.point.y};
         }));
         path.segments.map(function(segment) {
             new paper.Path.Circle({
@@ -81,7 +110,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     tool.onTouchEnd = function(event) {
         traces.push(path.segments.map(function(segment) {
-            return {x: Math.round(segment.point.x), y: Math.round(segment.point.y)};
+            return {x: segment.point.x, y: segment.point.y};
         }
         ));
         path.segments.map(function(segment) {
@@ -99,16 +128,90 @@ document.addEventListener('DOMContentLoaded', function() {
         );
     };
 
-    downloadButton.addEventListener('click', function() {
+    
+
+    document.getElementById('previewButton').addEventListener('click', function() {
         let inkML = exportToInkML(traces);
-        download('drawing.inkml', inkML);
+        let canvasSize = '' + canvas.getBoundingClientRect()['width'] + '/' + canvas.getBoundingClientRect()['height'];
+        fetch('http://127.0.0.1:5000/api/data', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({inkML: inkML, canvasSize: canvasSize})
+            
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data.result);
+            let shapes = get_shapes(data.result);
+            // document.getElementById('output').textContent = JSON.stringify(data.result);
+            let editButton = document.createElement('button');
+            editButton.id = 'editButton';
+            editButton.textContent = 'Edit';
+            document.querySelector('menu').appendChild(editButton);
+            svg.style.display = 'block';
+            canvas.style.display = 'none';
+            // add shapes to svg
+            for (let circle of shapes[0]) {
+                let circleElement = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+                circleElement.setAttribute('cx', circle.min_x);
+                circleElement.setAttribute('cy', circle.min_y);
+                circleElement.setAttribute('r', circle.width / 2);
+                circleElement.setAttribute('fill', 'none');
+                circleElement.setAttribute('stroke', 'black');
+                svg.appendChild(circleElement);
+            }
+            for (let rectangle of shapes[1]) {
+                let rectangleElement = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+                rectangleElement.setAttribute('x', rectangle.min_x);
+                rectangleElement.setAttribute('y', rectangle.min_y);
+                rectangleElement.setAttribute('width', rectangle.width);
+                rectangleElement.setAttribute('height', rectangle.height);
+                rectangleElement.setAttribute('fill', 'none');
+                rectangleElement.setAttribute('stroke', 'black');
+                svg.appendChild(rectangleElement);
+            }
+
+            let download_pnml_btn = document.createElement('button');
+            download_pnml_btn.id = 'download_pnml_btn';
+            download_pnml_btn.textContent = 'Download as pnml';
+            document.querySelector('menu').appendChild(download_pnml_btn);
+            document.getElementById('download_pnml_btn').addEventListener('click', function() {
+                console.log('download pnml', data.result);
+                
+                let pnml = exportToPnml(shapes);
+               
+                downloadInkml('petri-net.pnml', pnml);
+            });
+
+            document.getElementById('editButton').addEventListener('click', function() {
+                
+                // document.getElementById('output').textContent = '';
+                document.getElementById('editButton').remove();
+                document.getElementById('download_pnml_btn').remove()
+                svg.style.display = 'none';
+                canvas.style.display = 'block';
+                previewButton.style.display = 'block';
+                download_inkml_btn.style.display = 'block'
+                upload_inkml_btn.style.display = 'block'
+                
+               
+            });
+            
+            previewButton.style.display = 'none';
+            download_inkml_btn.style.display = 'none'
+            upload_inkml_btn.style.display = 'none'
+
     });
+
 
     document.getElementById('fileInput').addEventListener('change', function(event) {
         
         // Everytime a new file is uploaded, we want to have a clean canvas
         paper.project.clear();
-
+        traces = [];
+        
         const file = event.target.files[0];
         if (!file) {
           return;
@@ -120,15 +223,14 @@ document.addEventListener('DOMContentLoaded', function() {
             // Process the .inkml file content here
             const parser = new DOMParser();
             const xml = parser.parseFromString(fileContent, 'text/xml');
-            const traces = xml.querySelectorAll('trace');
-
+            const inkml_traces = xml.querySelectorAll('trace');
             let allPoints = [];
             // // Loop durch die Spuren und zeichne sie auf den Canvas
-            traces.forEach((trace) => {
+            inkml_traces.forEach((trace) => {
                 trace = trace.textContent.split(',').filter(item => item.trim() !== '');
                 const points = trace.map(point => {
-                    const [x, y, ...rest] = point.split(' ').filter(item => item.trim() !== '');                                       
-                    return new paper.Point(parseFloat(x), parseFloat(y)); 
+                const [x, y, ...rest] = point.split(' ').filter(item => item.trim() !== '');                                       
+                return new paper.Point(parseFloat(x), parseFloat(y)); 
                    
                 })
                 allPoints = allPoints.concat(points);
@@ -145,27 +247,38 @@ document.addEventListener('DOMContentLoaded', function() {
             const scaleX = canvas.getBoundingClientRect()['width'] / width;
             const scaleY = canvas.getBoundingClientRect()['height'] / height;
             const scale = Math.min(scaleX, scaleY);
-
             // mittig setzen
             const offsetX = (canvas.getBoundingClientRect()['width'] - width * scale) / 2;
             const offsetY = (canvas.getBoundingClientRect()['height'] - height * scale) / 2;
 
             
             // Then draw each trace with points transformed based on the calculated scale
-            traces.forEach(function(trace, i) {
+            inkml_traces.forEach(function(trace, i) {
                 const points = trace.textContent.trim().split(',').map(point => {
                     const [x, y] = point.split(' ').filter(item => item.trim() !== '');
                     const scaledX = (parseFloat(x) - minX) * scale + offsetX;
                     const scaledY = (parseFloat(y) - minY) * scale + offsetY;
                     return new paper.Point(scaledX, scaledY);
+                    // console.log(x, y);
+                    // return new paper.Point((parseFloat(x) - minX) * scale, (parseFloat(y) - minY) * scale);
                 });
+                // generate a random color
+                const color = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
                 // Create and draw the path
-                const path = new paper.Path({
+
+                let inkml_path = new paper.Path({
                     segments: points,
-                    strokeColor: 'black',
+                    strokeColor: color,
                     strokeWidth: 2,
                     fullySelected: false
                 });
+
+                
+                let tracePoints = inkml_path.segments.map(function(segment) {
+                    return {x: segment.point.x, y: segment.point.y};
+                });
+                traces.push(tracePoints);  // Save the trace in the global traces array
+            
 
                 // path.segments.map(function(segment) {
                 //     new paper.Path.Circle({
@@ -174,8 +287,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 //         fillColor: 'red'  // Color of the circle
                 // });
 
-                // // Calculate the middle point of the path
-                // let middlePoint = path.getPointAt(path.length / 2);
+                // // // Calculate the middle point of the path
+                // // let middlePoint = path.getPointAt(path.length / 2);
 
                 
                     
@@ -183,18 +296,23 @@ document.addEventListener('DOMContentLoaded', function() {
                 // });
 
                 // Add order of drawn strokes
-                // let text = new paper.PointText({
-                //     point: path.segments[0].point.add([0, -10]), // Position the text above the middle segment
-                //     content: '' + i, // The text content
-                //     fillColor: 'black',
-                //     fontFamily: 'Arial',
-                //     fontWeight: 'bold',
-                //     fontSize: 16
-                // }); 
+                // if(i == 60 || i == 61 || i == 62 || i ==63||i==64|| i==65 || i==95){
+                    let text = new paper.PointText({
+                        point: inkml_path.segments[0].point.add([0, -10]), // Position the text above the middle segment
+                        content: '' + i, // The text content
+                        fillColor: color,
+                        fontFamily: 'Arial',
+                        fontWeight: 'bold',
+                        fontSize: 16
+                    });
                 
+                    text.justification = 'center'; // Center the text horizontally
+                // }
+               
 
-                // text.justification = 'center'; // Center the text horizontally
+
             });
+            console.log('jsflsa', inkml_traces.length, traces.length);
 
             paper.view.draw();
             zoomOut();
@@ -204,11 +322,27 @@ document.addEventListener('DOMContentLoaded', function() {
     });  
  
 });
+    
+    });
 
+function get_shapes(recognition_result){
+    let circles = []
+    let rectangles = []
+    let lines = []
 
+    for (let item of recognition_result) {
+       if(item.shape_name === 'circle') {
+           circles.push(item)
+       }
+       else if(item.shape_name === 'rectangle') {
+           rectangles.push(item)
+       }
+       else if(item.shape_name === 'line') {
+           lines.push(item)
+       }
+    }
+    return [circles, rectangles, lines]
 
-
-
-
+}
 
 
